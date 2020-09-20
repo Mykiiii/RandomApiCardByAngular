1285f9412c3fc2724ae98401a1790becf21bebe9
Mayank <mohitmv0673@gmail.com>
2020-09-20 17:11:16 +0530

    API

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,14 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
-  });
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "./services/user.service";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'random-card';
+export class AppComponent implements OnInit {
+  title = "random-card";
+
+  user: any;
+
+  constructor(
+    private userService: UserService,
+    private toastr: ToastrService,
+  ) {}
+
+  ngOnInit() {
+    this.userService.getUser().subscribe(
+      (user: any) => {
+        console.log(user);
+        this.user = user.results[0];
+      },
+      (err) => {
+        this.toastr.error(err.status, "OOps");
+      },
+    );
+  }
 }
